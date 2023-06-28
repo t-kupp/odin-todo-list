c1ba154f2a30785cfdd2d08d7ea30bef6b816c81
t-kupp <kupper.thorge@gmail.com>
2023-06-28 15:37:16 +0200

    added more functionality

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const btnToday = document.querySelector("#btn-default-today");
 const btnWeek = document.querySelector("#btn-default-week");
 const btnAddProject = document.querySelector("#btn-add-project");
 const btnAddTask = document.querySelector("#btn-add-task");
+const btnSubmitForm = document.querySelector("#input-submit");
+const taskList = document.querySelector("#task-list");
+const btnImportant = document.querySelector("#input-important");
 
-// Array to store the tasks in
-let taskList = [];
+let myTasks = [];
 
 // Constructor to create new tasks and push them into the array
 function Task(title, description, project, date, isImportant, isDone) {
@@ -15,26 +17,27 @@ function Task(title, description, project, date, isImportant, isDone) {
   this.date = date;
   this.isImportant = isImportant;
   this.isDone = isDone;
-  taskList.push(this);
+  myTasks.push(this);
 }
 
+// Example task
 new Task(
   "Finish the To-Do List", //title
   "Complete this project to advance to the next part of the curriculum.", //description
   "home", //project folder
-  "", //date
+  "2023-07-01", //date
   true, //isImportant
   false //isDone
 );
 
-// Functionality for the 'Add Task' button
+// Clicking the Add Task button opens the new task form
 btnAddTask.addEventListener("click", () => {
-  openNewTaskForm();
+  toggleTaskFormVisibility("visible");
 });
 
-function openNewTaskForm() {
+function toggleTaskFormVisibility(v) {
   const formWrapper = document.querySelector("#form-wrapper");
-  formWrapper.style.visibility = "visible";
+  formWrapper.style.visibility = v;
 
   formWrapper.addEventListener("click", (e) => {
     if (e.target == formWrapper) {
@@ -44,9 +47,92 @@ function openNewTaskForm() {
   });
 }
 
-//delete
-const submitBtn = document.querySelector("#input-submit");
-submitBtn.addEventListener("click", () => {
-  const formWrapper = document.querySelector("#form-wrapper");
-  formWrapper.style.visibility = "hidden";
+// Add new task to array when submitting the form
+
+btnSubmitForm.addEventListener("click", () => {
+  const title = document.querySelector("#input-title");
+  const description = document.querySelector("#input-description");
+  const date = document.querySelector("#input-date");
+
+  if (title.value.trim() === "") {
+    alert("Please enter a title.");
+    return;
+  }
+
+  new Task(title.value.trim(), description.value.trim(), "", date.value, isImportant, false);
+
+  title.value = "";
+  description.value = "";
+  date.value = "";
+  btnImportant.classList.remove("toggled");
+  isImportant = false;
+
+  toggleTaskFormVisibility("hidden");
+
+  drawTasksFromArray();
 });
+
+// Draw tasks from the array
+function drawTasksFromArray() {
+  taskList.innerHTML = "";
+
+  for (let i = 0; i < myTasks.length; i++) {
+    const newTaskWrapper = taskList.appendChild(document.createElement("div"));
+    newTaskWrapper.classList.add("task-wrapper");
+
+    const newLeftSide = newTaskWrapper.appendChild(document.createElement("div"));
+    newLeftSide.classList.add("left-side");
+
+    const newRightSide = newTaskWrapper.appendChild(document.createElement("div"));
+    newRightSide.classList.add("right-side");
+
+    const newTitle = newLeftSide.appendChild(document.createElement("p"));
+    newTitle.textContent = myTasks[i].title;
+    newTitle.classList.add("task-title");
+
+    const newDescription = newLeftSide.appendChild(document.createElement("p"));
+    newDescription.textContent = myTasks[i].description;
+    newDescription.classList.add("task-description");
+
+    const newImportant = newRightSide.appendChild(document.createElement("p"));
+    newImportant.classList.add("task-important");
+    if (myTasks[i].isImportant == true) {
+      newImportant.textContent = "!!!";
+    }
+
+    const newDate = newRightSide.appendChild(document.createElement("p"));
+    newDate.textContent = myTasks[i].date;
+    newDate.classList.add("task-date");
+
+    const newIsDone = newRightSide.appendChild(document.createElement("input"));
+    newIsDone.type = "checkbox";
+    newIsDone.checked = myTasks[i].isDone;
+    newIsDone.classList.add("task-done");
+
+    const newDeleteButton = newRightSide.appendChild(document.createElement("input"));
+    newDeleteButton.type = "button";
+    newDeleteButton.value = "DEL";
+    newDeleteButton.classList.add("task-delete");
+    newDeleteButton.addEventListener("click", () => {
+      myTasks.splice(i, 1);
+      drawTasksFromArray();
+    });
+  }
+}
+drawTasksFromArray();
+
+// Functionality for the "Important?"" button
+btnImportant.addEventListener("click", () => {
+  toggleImportantStatus();
+});
+
+let isImportant = false;
+function toggleImportantStatus() {
+  if (isImportant == true) {
+    isImportant = false;
+    btnImportant.classList.remove("toggled");
+    return;
+  }
+  btnImportant.classList.add("toggled");
+  isImportant = true;
+}
